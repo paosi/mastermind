955458eab32de9837033935a9a7f8064d2eb2689
“paosi” <paosi@gmx.com>
2022-04-28 01:17:06 -0400

    Add sound effects.

--- mastermind.py
+++ mastermind.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python3
 
 import requests
+import os
+from playsound import playsound
 
 
 # Print instructions
 def intro():
 
+  sounds = os.path.join(os.getcwd(), "sounds", "")
+  
   print("-------------------------------------------------------------------------------------")
   print("**   **     **     ****  *******  ******  ******   **   **  *******  **    *  ****** ")
   print("* * * *    *  *   **        *     *       *     *  * * * *     *     * *   *  *     *")
@@ -13,11 +17,14 @@ def intro():
   print("*     *  *      *     **    *     *       *     *  *     *     *     *   * *  *     *")
   print("*     *  *      *  ****     *     ******  *     *  *     *  *******  *    **  ****** ")
   print("-------------------------------------------------------------------------------------\n")
-  print("INSTRUCTIONS:\n")
-  print("Try to guess the 4 digit combination.")
-  print("The numbers are in the range 0-7.")
-  print("You have 10 tries.")
-  print("Good luck!\n")
+  print("                                   INSTRUCTIONS:\n")
+  print("                     ------------------------------------------")
+  print("                     |  Try to guess the 4 digit combination. |")
+  print("                     |    The numbers are in the range 0-7.   |")
+  print("                     |           You have 10 tries.           |")
+  print("                     |               Good luck!               |")
+  print("                     ------------------------------------------\n")
+  playsound(sounds + "001.mp3")
 
   return
 
@@ -39,30 +46,47 @@ def get_random(url):
 # Asks the user for a 4 number guess and compares it to the random numbers.
 def mastermind(winning_nums):
 
+  sounds = os.path.join(os.getcwd(), "sounds", "")
+
   #print(winning_nums, "\n")
-  print("Guess the combination. You have 10 tries.")
+  print("Guess the combination. You have 10 tries remaining.")
+  print("---------------------------------------------------")
+  playsound(sounds + "002.wav")
 
   i = 10
   possible_nums = ["0", "1", "2", "3", "4", "5", "6", "7"]
+  ignore = ["-", ",", ".", "/", " "]
 
   while i > 0:
+
     guess_list = []
+
+    # Get user input and remove whitespace, comma, period, etc., if any.
     guess = input("Choose any combination of 4 numbers from 0-7 --> ")
+    playsound(sounds + "002.wav")
+    guess_copy = guess
+    for g in guess_copy:
+      if g in ignore:
+        guess =  guess.replace(g, "")
 
-    for num in guess:
-      if num not in possible_nums:
-        print(f"{num} is an invalid guess. Choose 4 numbers between 0-7.")
+    # Make sure user inputs are valid numbers.
+    for char in guess:
+      if char not in possible_nums:
+        print(f"{char} is an invalid guess. Choose 4 numbers between 0-7.")
         continue
-      guess_list.append(num)
-    
-    print(guess_list)
+      guess_list.append(char)
 
+    # Make sure user chooses 4 valid numbers only.
     if len(guess_list) != 4:
       print("Must choose 4 numbers. Try again.")
+      playsound(sounds + "004.mp3")
       continue
 
+    print(guess_list)
+
     if guess_list == winning_nums:
-      print("YOU WON YOU LUCKY SON OF A BITCH!")
+      print("YOU ARE A WINNER!")
+      playsound(sounds + "005.wav")
       return
 
     # Functionality for exact and partially correct guesses here.
@@ -101,25 +125,22 @@ def mastermind(winning_nums):
         x = "numbers"
 
       print(f"You got {partial + exact} {x} correct and {exact} {y} in the exact spot.")
+      if (partial + exact) > 0:
+        playsound(sounds + "003.mp3")
 
       i -= 1
-      print(f"Wrong. You have {i} tries left")
+      print(f"Try again. You have {i} tries remaining.")
+      print("---------------------------------------------------")
 
   print("You lost. Big time.")
+  playsound(sounds + "006.wav")
   return
 
-
-# Guess history and computer feedback function.
-def get_history():
-
-  history = "Hello!!!"
-
-  return history
 
 
 if __name__ == "__main__":
 
-  intro()
   url = "https://www.random.org/integers/?num=4&min=0&max=7&col=1&base=10&format=plain&rnd=new"
-  winning_nums = get_random(url)
-  mastermind(winning_nums)
+  
+  intro() 
+  mastermind(get_random(url))
