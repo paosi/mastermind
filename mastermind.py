f219b2ecde436b9f7af7b3bdc505fcbf99a64fa0
“paosi” <paosi@gmx.com>
2022-04-29 01:24:54 -0400

    Refined game and interface.

--- mastermind.py
+++ mastermind.py
@@ -24,11 +24,13 @@ def intro():
   print("-------------------------------------------------------------------------------------\n")
   print("                                   INSTRUCTIONS:\n")
   print("                     ------------------------------------------")
-  print("                     |  Try to guess the 4 digit combination. |")
+  print("                     |  Try to guess the 4 digit combination  |")
   print("                     |    The numbers are in the range 0-7.   |")
   print("                     |           You have 10 tries.           |")
   print("                     |               Good luck!               |")
   print("                     ------------------------------------------\n")
+  print("                                  EXIT: 'Control-C'")
+  print("                                 VIEW HISTORY: 'log'\n")
   playsound(sounds + "001.mp3")
 
   return
@@ -53,22 +55,36 @@ def mastermind(winning_nums):
 
   sounds = os.path.join(os.getcwd(), "sounds", "")
 
-  #print(winning_nums, "\n")
+  #print(winning_nums)
+  print("\n")
   print("Guess the combination. You have 10 tries remaining.")
-  print("---------------------------------------------------")
+  print("------------------------------------------------------------------------------------")
   playsound(sounds + "002.wav")
 
   i = 10
+  count = 1
   possible_nums = ["0", "1", "2", "3", "4", "5", "6", "7"]
   ignore = ["-", ",", ".", "/", " "]
+  log = []
 
   while i > 0:
 
     guess_list = []
+    log_dict = {}
 
     # Get user input and remove whitespace, comma, period, etc., if any.
-    guess = input("Choose any combination of 4 numbers from 0-7 --> ")
+    guess = input(f"Try #{count} - Choose 4 numbers from 0-7 --> ")
     playsound(sounds + "002.wav")
+
+    if guess.lower() == "log":
+      print("\n")
+      print("===================================================")
+      for l in log:        
+        print(l)
+      print("===================================================\n")
+      playsound(sounds + "007.wav")
+      continue
+
     guess_copy = guess
     for g in guess_copy:
       if g in ignore:
@@ -83,18 +99,27 @@ def mastermind(winning_nums):
 
     # Make sure user chooses 4 valid numbers only.
     if len(guess_list) != 4:
-      print("Must choose 4 numbers. Try again.")
+      print("ERROR - Must choose 4 valid numbers. Try again.\n")
       playsound(sounds + "004.mp3")
       continue
 
+    print("\n")
     print(guess_list)
 
     if guess_list == winning_nums:
-      print("YOU ARE A WINNER!")
+      match count:
+        case 1:
+          nth = "st"
+        case 2:
+          nth = "nd"
+        case 3:
+          nth = "rd"
+        case _:
+          nth = "th"
+      print(f"YOU ARE A WINNER! YOU GOT IT ON YOUR {count}{nth} TRY!")
+      print("------------------------------------------------------------------------------------\n")
       playsound(sounds + "005.wav")
       return
-
-    # Functionality for exact and partially correct guesses here.
     else:
       exact = 0
       partial = 0
@@ -124,20 +149,35 @@ def mastermind(winning_nums):
       else:
         y = "are"
 
-      if partial == 1:
+      if partial + exact == 1:
         x = "number"
       else:
         x = "numbers"
 
-      print(f"You got {partial + exact} {x} correct and {exact} {y} in the exact spot.")
+      print(f"You got {partial + exact} {x} correct and {exact} {y} in the exact spot.\n")
       if (partial + exact) > 0:
         playsound(sounds + "003.mp3")
 
+      log_dict[count] = guess_list
+      log_dict["Correct"] = partial = exact
+      log_dict["Exact"] = exact
+      log.append(log_dict)
       i -= 1
-      print(f"Try again. You have {i} tries remaining.")
-      print("---------------------------------------------------")
+      count += 1
 
-  print("You lost. Big time.")
+      # Print formatting
+      if i == 1:
+        tries = "try"
+      else:
+        tries = "tries"
+
+      if i == 0:
+        continue
+      print(f"Try again. You have {i} {tries} remaining.")
+      print("------------------------------------------------------------------------------------")
+
+  print(f"You lost. Better luck next time!                     Solution: {winning_nums}")
+  print("-------------------------------------------------------------------------------------\n")
   playsound(sounds + "006.wav")
   return
 
